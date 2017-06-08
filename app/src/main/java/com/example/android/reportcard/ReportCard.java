5d3fd40745171ed658811296be9fb15866719114
francisgalvez <francisgalvezlao@gmail.com>
2017-06-08 11:55:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/reportcard/ReportCard.java
@@ -0,0 +1,61 @@
+package com.example.android.reportcard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Francis Gálvez on 08/06/2017.
+ */
+
+public class ReportCard {
+    private String studentName;
+    private String studentYear;
+    private HashMap<String, Integer> grades;
+
+    public ReportCard(String studentName, String studentYear){
+        this.studentName = studentName;
+        this.studentYear = studentYear;
+        this.grades = new HashMap<String, Integer>();
+    }
+    
+    public ReportCard(String studentName, String studentYear, HashMap<String, Integer> grades) {
+        this.studentName = studentName;
+        this.studentYear = studentYear;
+        this.grades = grades;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentYear() {
+        return studentYear;
+    }
+
+    public void setStudentYear(String studentYear) {
+        this.studentYear = studentYear;
+    }
+
+    public HashMap<String, Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(HashMap<String, Integer> grades) {
+        this.grades = grades;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer(studentName + ", who is studying " + studentYear + " got this grades:\n");
+
+        for(Map.Entry<String, Integer> it : grades.entrySet()){
+            sb.append(it.getKey() + ": " + it.getValue() + "\n");
+        }
+
+        return sb.toString();
+    }
+}
